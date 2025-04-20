d49e5e4e3cb9848f560f92ffed2a3fd47a9548c7
Ilbhihii <128502713+Ilbhihii@users.noreply.github.com>
2025-04-20 23:05:32 +0100

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -5,11 +5,11 @@ const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        baseUrl: 'article-extractor-and-summarizer.p.rapidapi.com/summarize-text',
         baseUrl: 'https://google-translation-unlimited.p.rapidapi.com',
         prepareHeaders: (headers) => {
-            headers.set('x-rapidapi-host', 'article-extractor-and-summarizer.p.rapidapi.com');
-            headers.set('x-rapidapi-host', 'google-translation-unlimited.p.rapidapi.com');
+            headers.set('x-rapidapi-host: article-extractor-and-summarizer.p.rapidapi.com');
+            headers.set('x-rapidapi-host: google-translation-unlimited.p.rapidapi.com');
             headers.set('x-rapidapi-key', '0e369d52b7msh9eea79c83313315p131b35jsnb515c61cd7b7');
         },
     }),
