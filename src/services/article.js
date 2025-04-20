f726aac708ab9e1d5f2ad65165f57d988c542e75
Ilbhihii <128502713+Ilbhihii@users.noreply.github.com>
2025-04-20 13:39:17 +0100

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -6,8 +6,10 @@ export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        baseUrl: 'https://google-translation-unlimited.p.rapidapi.com',
         prepareHeaders: (headers) => {
             headers.set('x-rapidapi-host', 'article-extractor-and-summarizer.p.rapidapi.com');
+            headers.set('x-rapidapi-host', 'google-translation-unlimited.p.rapidapi.com');
             headers.set('x-rapidapi-key', '0e369d52b7msh9eea79c83313315p131b35jsnb515c61cd7b7');
         },
     }),
